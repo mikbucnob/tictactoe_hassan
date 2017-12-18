93dfe09657223c9a905466142d14a7c1b9a38229
Michael <mikbucnob@yahoo.com.au>
2017-12-18 18:08:09 +1100

    First commit

---
+++ TicTacToeHassan/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace TicTacToeHassan
+{
+    partial class GameScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.player2ScoreTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.player1ScoreTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(982, 749);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn1.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.Lime;
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(321, 243);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "O";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn2.AutoSize = true;
+            this.btn2.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.Red;
+            this.btn2.Location = new System.Drawing.Point(330, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(321, 243);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "X";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn4.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(3, 252);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(321, 243);
+            this.btn4.TabIndex = 2;
+            this.btn4.Text = "O";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn5.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(330, 252);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(321, 243);
+            this.btn5.TabIndex = 3;
+            this.btn5.Text = "X";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn3.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(657, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(322, 243);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "X";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn6.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(657, 252);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(322, 243);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "O";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn7.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(3, 501);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(321, 245);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "X";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn8.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(330, 501);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(321, 245);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "O";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn9.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(657, 501);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(322, 245);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "X";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(30, 30, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(30);
+            this.label1.Size = new System.Drawing.Size(881, 199);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TicTacToe Game";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.player2ScoreTxt);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.player1ScoreTxt);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(982, 953);
+            this.splitContainer1.SplitterDistance = 200;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(756, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Computer Score:";
+            // 
+            // player2ScoreTxt
+            // 
+            this.player2ScoreTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.player2ScoreTxt.Location = new System.Drawing.Point(876, 170);
+            this.player2ScoreTxt.Name = "player2ScoreTxt";
+            this.player2ScoreTxt.Size = new System.Drawing.Size(54, 22);
+            this.player2ScoreTxt.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(34, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Player1 Score:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // player1ScoreTxt
+            // 
+            this.player1ScoreTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.player1ScoreTxt.Location = new System.Drawing.Point(59, 170);
+            this.player1ScoreTxt.Name = "player1ScoreTxt";
+            this.player1ScoreTxt.Size = new System.Drawing.Size(54, 22);
+            this.player1ScoreTxt.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(119, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Player Score:";
+            // 
+            // GameScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "GameScreen";
+            this.Text = "TicTacToe";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button[] btns;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox player2ScoreTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox player1ScoreTxt;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TicTacToeHassan/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeHassan
+{
+    public partial class GameScreen : Form
+    {
+        bool isComputerTurn = false;
+        Dictionary<Button,EventHandler> btns_evt_handlers = new Dictionary<Button, EventHandler>();
+        //(different Buttons) key is unique and EventHandler is a delegate points to same method(not unique)
+
+        List<int[]> winningCombos;
+        int emptySpots;
+        int player_score;
+        int computer_score;
+
+
+        private Button RandomComputerChoice()
+        {
+            Random a = new Random();
+            int randomChoice = a.Next(0, 8);
+            Button computerChosenBtn = btns[randomChoice];
+
+            while (computerChosenBtn.Text != "") { 
+                //computerChosenBtn = btns[4] /*middle spot*/ ;
+                randomChoice = a.Next(0, 8);
+                computerChosenBtn = btns[randomChoice];
+            }
+            return computerChosenBtn;
+        }
+
+        private void play_a_turn(object sender, EventArgs e)
+        {
+            Console.WriteLine("You played a turn");
+            Button btn = sender as Button;//cast sender as button if btn is anything else than button null is used
+            if(isComputerTurn) //computer always X
+            {
+                btn.ForeColor = System.Drawing.Color.Red;
+                btn.Text = "X";
+                btn.Click -= btns_evt_handlers[btn];
+                emptySpots--;
+                
+                isComputerTurn = !isComputerTurn;
+                //btn.Enabled = false;
+                //btn.Anchor 
+                //isComputerTurn = false;
+            }
+            else //player's turn
+            {
+
+                btn.ForeColor = System.Drawing.Color.Lime;
+                btn.Text = "O";
+                btn.Click -= btns_evt_handlers[btn];
+                emptySpots--;
+
+                isComputerTurn = !isComputerTurn;
+
+                if (emptySpots > 0)
+                {
+                    btn = RandomComputerChoice();
+                    btn.Invoke(btns_evt_handlers[btn]);
+                }
+                // btn.Enabled = false;
+                //isComputerTurn = true;
+            }
+           // isComputerTurn = !isComputerTurn;//not boolean of computer's turn
+
+            // a play has been made
+
+            bool player_won = false;
+            bool computer_won = false;
+            foreach (int[] winningCombo in winningCombos)
+            {
+                int Index1 = winningCombo[0];
+                int Index2 = winningCombo[1];
+                int Index3 = winningCombo[2];
+                if (btns[Index1].Text == "O" && btns[Index2].Text == "O" && btns[Index3].Text == "O")
+                {
+                    Console.WriteLine("Player won ! " + Index1 + Index2 + Index3);
+                    player_won = true;
+                    break;
+                }
+            }
+
+            if ( !player_won)
+            {
+
+                foreach (int[] winningCombo in winningCombos)
+                {
+                    int Index1 = winningCombo[0];
+                    int Index2 = winningCombo[1];
+                    int Index3 = winningCombo[2];
+                    if (btns[Index1].Text == "X" && btns[Index2].Text == "X" && btns[Index3].Text == "X")
+                    {
+                        Console.WriteLine("Computer won ! " + Index1 + Index2 + Index3);
+                        computer_won = true;
+                        break;
+                    }
+                }
+            }
+
+            if(player_won)
+            {
+                player_score++;
+                MessageBox.Show("Player Won !");
+                player1ScoreTxt.Text = "" + player_score;
+
+                clear_board();
+            }else if (computer_won)
+            {
+                 
+                computer_score++;
+                MessageBox.Show("Computer Won !");
+                player2ScoreTxt.Text = "" + computer_score;
+
+                clear_board();
+            }else if(emptySpots == 0)
+            {
+
+                MessageBox.Show("A Draw !");
+                clear_board();
+            }
+
+        }
+
+        private void clear_board()
+        {
+            this.tableLayoutPanel1.Controls.Clear();
+            emptySpots = 9;
+            isComputerTurn = false;
+
+
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    int i = r * 3 + c;
+                    btns[i] = new Button();
+                    btns[i].Text = "";
+
+                    btns[i].AutoSize = true;
+                    btns[i].Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    btns[i].ForeColor = System.Drawing.Color.Red;
+                    btns[i].Location = new System.Drawing.Point(330, 3);
+                    btns[i].Size = new System.Drawing.Size(321, 243);
+                    btns[i].TabIndex = 1 + i;
+                    btns[i].UseVisualStyleBackColor = true;
+
+                    btns_evt_handlers[btns[i]] = new System.EventHandler(this.play_a_turn);
+                    btns[i].Click += btns_evt_handlers[btns[i]];
+                    this.tableLayoutPanel1.Controls.Add(this.btns[i], r, c);
+                }
+            }
+        }
+
+        public GameScreen()
+        {
+            btns = new Button[9];
+
+            winningCombos = new List<int[]>();
+            winningCombos.Add(new int[] { 0, 1, 2 }); //012 - 1
+            winningCombos.Add(new int[] { 3, 4, 5 }); //345 - 2
+            winningCombos.Add(new int[] { 6, 7, 8 }); //678 - 3
+            winningCombos.Add(new int[] { 0, 3, 6 }); //036 - 4
+            winningCombos.Add(new int[] { 1, 4, 7 }); //147 - 5
+            winningCombos.Add(new int[] { 2, 5, 8 }); //258 - 6
+            winningCombos.Add(new int[] { 0, 4, 8 }); //048 - 7
+            winningCombos.Add(new int[] { 2, 4, 6 }); //246 - 8 (there should be 8
+
+            player_score = 0;
+            computer_score = 0;
+
+            // can put buttons in array
+            //Initialize UI
+            InitializeComponent();
+
+            this.tableLayoutPanel1.Controls.Clear();
+
+            player1ScoreTxt.Text = "0";
+            player2ScoreTxt.Text = "0";
+
+            clear_board();
+
+            
+
+
+            //Start the Game
+
+            this.btn2.ForeColor = System.Drawing.Color.Blue;
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            //array avoids this
+            //this.btn1.Text = "";
+            //this.btn2.Text = "";
+            //this.btn3.Text = "";
+            //this.btn4.Text = "";
+            //this.btn5.Text = "";
+            //this.btn6.Text = "";
+            //this.btn7.Text = "";
+            //this.btn8.Text = "";
+            //this.btn9.Text = "";
+
+            Boolean keepPlaying = true;
+            while (keepPlaying)
+            {
+                // clear all boxes..
+                // reset color to black
+
+                //check for winners /losers
+                //keep track of score
+                keepPlaying = false;
+
+            }
+            Console.WriteLine("Thank you for playing");
+        }
+
+    }
+}
